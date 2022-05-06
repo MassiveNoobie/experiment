edb2366438e49ad6fbab14175674a3a8731c6301
T <32969167+MassiveNoobie@users.noreply.github.com>
2022-05-05 21:15:26 -0500

    commit on master

--- learning16 - arrays.js
+++ learning16 - arrays.js
@@ -1,4 +1,4 @@
-//array
+//array - almost all days of week, we use push later in script to add more to the array
 const daysOfWeek = [
     "monday",
     "tuesday",
@@ -23,24 +23,32 @@ const delimit = "  {}  "
 //log length of array
 //length is a property* doesn't require (), otherwise it's a function
 console.log(daysOfWeek.length)
+
 //log 1 string from all of the values in the days of week array, adds ability to add a delimiter
 // defaults to ","
 // join is a function*
 console.log(daysOfWeek.join())
+
 // swapping to pipe
 console.log(daysOfWeek.join("|"))
+
 //log days of week with a start text and end text & upper case
 console.log(leftStart+daysOfWeek.join(",").toUpperCase()+rightEnd)
+
 //log everything with upper case & using variables across everything
 console.log(leftStart+daysOfWeek.join(delimit).toUpperCase()+rightEnd)
 
 
 
 
-//push
+//push function - adds value to end of array
 daysOfWeek.push("saturday")
 console.log(daysOfWeek.join("|"))
+
+//push function - adds value to end of array
 daysOfWeek.push("sunday")
 console.log(daysOfWeek.join("|"))
+
+//reverse function - reverse order of array
 daysOfWeek.reverse()
 console.log(daysOfWeek.join("|"))
