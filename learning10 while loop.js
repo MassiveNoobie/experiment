a81e5238bdd537d3eb576000edaebf1c51e47f87
T <32969167+MassiveNoobie@users.noreply.github.com>
2022-05-06 16:48:08 -0500

    commit on master

---
+++ learning10 while loop.js
@@ -0,0 +1,24 @@
+// start loop on zero
+let x = 0
+
+//loop ten times
+while(x<18){
+    //log +1 each loop
+    console.log(x)
+   
+    //by passing this line you're increasing the loop count by 1
+    x=x+1
+   
+    //compressed version of above line, increasing loop count by 2 each loop
+    x++
+   
+    //remove1
+    //x--
+   
+    //remove1
+    //--x
+   
+    //add1
+    //++x
+}
+console.log(x)
--- learning14 builtins.js
+++ learning14 builtins.js
@@ -1,8 +1,11 @@
 // a string with weird casing
 const sentence = "ThIs HaS WiRed"
+
 // lowercase
 console.log(sentence.toLowerCase())
+
 // uppercase
 console.log(sentence.toUpperCase())
+
 // round decimal place
 console.log(Math.round(5.121212))
